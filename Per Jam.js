3fa03bdc25377753b4b836fc6b16f658e9771526
farrelzna <farrelizzanmaulana@smkwikrama.sch.id>
2024-01-04 11:42:03 +0700

    commit KE tiga

---
+++ Per Jam.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+//   var tg = new Date();
+//   var jm = tg.getHours();
+//   if (jm > 5 && jm <= 8)
+//   document.body.className = "pagi";
+//   else if (jm > 8 && jm <= 15 )
+//   document.body.className = "siang";
+//   else if (jm > 15 && jm <= 18)
+//   document.body.className = "sore";
+//   else if (jm > 18 || jm <= 5)
+//   document.body.className = "malam";
+//   });
