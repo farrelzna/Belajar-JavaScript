ba02483867163d04b049043cd4fb7a9d6d577c78
farrelzna <farrelizzanmaulana@smkwikrama.sch.id>
2024-01-04 09:49:31 +0700

    commit KE pertama

---
+++ main.js
@@ -0,0 +1,355 @@
+// nilai
+
+// let PABP = 70;
+// let Math = 82;
+// let DPK = 77;
+// let rata2 = (PABP + Math + DPK) / 3;
+
+// if (rata2 >= 80 && rata2 <= 100) {
+//     grade = "A"
+// }
+// else if (rata2 >= 75 && rata2 <= 79) {
+//     grade = "B"
+// }
+// else if (rata2 >= 65 && rata2 <= 74) {
+//     grade = "C"
+// }
+// else if (rata2 >= 45 && rata2 <= 64) {
+//     grade = "D"
+// }
+// else if (rata2 >= 0 && rata2 <= 44) {
+//     grade = "E"
+// }
+// if {
+//     grade = "K"
+// }
+// console.log(rata2);
+// console.log(grade);
+
+// gatau
+
+// let a = '80';
+// let b = '90';
+// console.log(a + b);
+// let c = true;
+// console.log(typeof(c))
+
+// usia
+
+// let usia = prompt('Umur kamu berapa sih?');
+
+// if (usia >= 18 && usia <90){
+//   if (usia >= 50){
+//   console.log('udah tua blog')
+//   } else {
+//     console.log ('Masi muda mas!')
+//   }
+// }
+// else if (usia > 15){
+//   console.log('Bocah ngapain kesini anjir')
+// }
+// else {
+//   console.log('Kekecilan ah')
+// }
+
+// ganjil genap
+
+// let angka = 8;
+
+// if(angka %2==0){
+//     console.log('genap')
+// }
+// else{
+//     console.log('ganjil')
+// }
+
+// gatau
+
+// let po = true;
+// if( po == true){
+//     console.log('benar')
+// }
+// else{
+//     console.log('salah')
+// }
+
+// // donat
+
+// let donat = false;
+
+// if (donat == true)[
+//   console.log('masuk')
+// ]
+// else {
+//   console.log('gak masuk')
+// }
+
+// absen
+
+// let hadir = false ;
+// let keterangan = "izin";
+
+// if (hadir == true){
+//     console.log("masuk")
+// }
+
+// else{
+
+//       if (keterangan == "sakit"){
+//         console.log("aku sakit")
+//       }
+//       else if (keterangan == "izin"){
+//         console.log('aku izin')
+//       }
+//       else{
+//         console.log('aku hilang')
+//       }
+// }
+
+// // admin
+
+// let login = true;
+// let role = '';
+
+// if ( login == true ){
+//   if(role == 'admin'){
+//     console.log('kamu admin sekarang!')
+
+//   } else if (role == 'moderator'){
+//       console.log ('selamat datang moderator!')
+//   } else { 
+//     console.log('selamat datang user!')
+//   }
+// } else {
+//   console.log('Gagal Cui!')
+// }
+
+// // gitulah
+
+// let bill= 80;
+// let bill2= 80;
+
+// if (bill > bill2){
+//   console.log(bill);
+// } 
+// else if (bill == bill2){
+//   console.log('sama kayny');
+// } 
+// else {
+//   console.log(bill2);
+// }
+
+
+// Input nilai
+
+// let jalur = 'prestasi';
+// let ini = 'rapot';
+// let nilai = 70;
+
+// if (nilai >= 89 && nilai <= 100){
+//     console.log ('Anda diterima dengan Nilai Sempurna')  
+//   }
+
+// else if (nilai >= 68 && nilai <=88){
+//   if (ini = 'rapot' && nilai >= 82){
+//     console.log ('Anda diterima melalui jalur rapot')
+//   }
+//   else if (jalur = 'prestasi' && nilai >= 75){
+//     console.log ('Anda diterima dengan jalur prestasi')
+//   }
+//   else {
+//     console.log ('Anda perlu test')
+//   }
+// }
+// else {
+//     console.log ('Anda tidak diterima')
+// }
+
+// harga
+
+// let baju = 5000;
+// let celana = 7000;
+// let sepatu = 3000;
+
+// let harga = (baju + celana + sepatu);
+// if (harga > 100000) {
+//     let potongan = harga * 10 / 100;
+//     let hargadiskon = harga - potongan;
+//     console.log(hargadiskon)
+
+// } else {
+//     console.log(harga)
+// }
+
+// bioskop
+
+// let vip = 50;
+// let ekonomi = 50;
+// let esekusi = 50;
+
+// gtw
+
+// let tinggiAir = prompt('ph');
+
+//         if (tinggiAir <= 500.0) {
+//             console.log ("aman kaynya")
+//         } else if (tinggiAir >= 500.1 && tinggiAir <= 600.0) {
+//             console.log ("hati hati aja sih kalo kata gw mah")
+//         } else if (tinggiAir >= 600.1 && tinggiAir <= 650.0) {
+//             console.log ("Siaga part 2")
+//         } else {
+//             console.log ("Siaga part 1")
+//         }
+
+// gaji bersih
+
+// let gajipokok = 1500000;
+// let tunjangan = 500000;
+// let anak = 4;
+// let bpjspajak = 8/100
+
+// if (anak == 1){
+//     console.log(gajipokok + tunjangan + 100000 + bpjspajak)
+// }
+// else if (anak > 2){
+//     console.log(gajipokok + tunjangan + 100000 + 100000 + bpjspajak)
+// }
+// else {
+//     console.log(gajipokok + tunjangan + bpjspajak)
+// }
+
+// looping
+
+// for (let i = 20; i >= 1; i--){
+//     document.write (i + '<br>')
+// }
+// for (let bensin = 500; bensin>0; bensin--){
+//     document.write('halo')
+// }
+
+// insiali
+
+// bensin = 1;
+
+// while(bensin <= 10){
+//     document.write('<h1>Cui</h1>' + '<br>')
+
+//     bensin++;
+// }
+
+// for (let i = 40; i=1; i--){
+//     console.log(i)
+// }
+
+// angka = 40;
+
+// while( angka>=1){
+//     document.write(angka)
+
+//     angka --;
+// }
+
+// while
+
+// let i = 100 
+
+// while (i = 1){
+//   if (i % 2 == 1){
+//   document.write(i + 'Bilangan genap <br>')
+//   } i--
+// }
+
+// faktorial
+
+// function faktorial(n) {
+//   if (n === 0 || n === 1) {
+//     return 1;
+//   } else {
+//     return n * faktorial(n - 1);
+//   } 
+// }
+
+// faktorial part2
+// let angka = 23;
+// let faktorial = 1;
+
+// for ( let i = 1; i <= angka; i++) {
+//   faktorial *=i;
+// }
+
+// console.log('faktorial angka ' + angka + ' adalah: ' + faktorial)
+
+// for 1 -100
+
+// for(let i = 1; i <= 100; i++){
+//   document.write(i)
+//   if(i%2 == 0){
+//       document.write(i+"Bilangan Genap" + "<br>")
+//   }else {
+//   document.write(i+"Bilangan Ganjil" + "<br>")
+//   }
+// }
+
+//for 100-1
+
+// for(let i = 100; i >= 1; i--){
+//     document.write(i)
+//     if(i%2 == 0){
+//         document.write(i+"Bilangan Genap" + "<br>")
+//     }else {
+//     document.write(i+"Bilangan Ganjil" + "<br>")
+// }
+// }
+
+// while 100-1
+
+// i = 100; 
+// while (i >= 1) {
+//     if (i%2 == 0){
+//     document.write(i+"Bilangan Genap" + "<br>");
+// }else {
+//     document.write(i+"Bilangan Ganjil" + "<br>");
+// }
+// i--;
+// }
+
+// while 1-100
+
+// i = 1; 
+// while (i <= 300) {
+//     if (i%2 == 0){
+//     document.write(i+"Bilangan Genap" + "<br>");
+// }else {
+//     document.write(i+"Bilangan Ganjil" + "<br>");
+// }
+// i++,
+
+// color per jam
+
+// $(document).ready(function(){
+//   var tg = new Date();
+//   var jm = tg.getHours();
+//   if (jm > 5 && jm <= 8)
+//   document.body.className = "pagi";
+//   else if (jm > 8 && jm <= 15 )
+//   document.body.className = "siang";
+//   else if (jm > 15 && jm <= 18)
+//   document.body.className = "sore";
+//   else if (jm > 18 || jm <= 5)
+//   document.body.className = "malam";
+//   });
+
+// tahun kabisat
+
+// document.write('<h1>kabisat apa ngga hayo</h1> <br>');
+// for (let i = 1600; i <= 2024; i++){
+//   if( i % 4 !=0){
+//     document.write(i + '<span> bukan kabisat!</span><br>')
+//   } else if (i % 100 !=0){
+//     document.write(i + '<span> merupakan tahun kabisat!</span><br>')
+//   } else if (i % 400 !=0){
+//     document.write(i + '<span> bukan kabisat!</span><br>')
+//   } else {
+//     document.write(i + '<span> merupakan tahun kabisat!</span><br>')
+//   }
+// }
